cf26af6e34cae09ea916d6ad934d7c25acd5aaa7
univem <univem@univem.edu.br>
2019-05-14 22:07:31 -0300

    criacao das instrucoes jmp

--- hd05/hc05.vhd
+++ hd05/hc05.vhd
@@ -84,7 +84,7 @@ begin
 							ESTADO <= EXECUTA;
 						END IF;
 						
-					WHEN "10100111" =>
+					WHEN "10100111" => 
 						
 						IF FASE = "00" THEN
 							PC <= PC + 1;
@@ -100,7 +100,49 @@ begin
 							PC <= AUX;							
 							ESTADO <= EXECUTA;
 						END IF;				
-				
+					
+					WHEN "00111010" => -- JMP - 3A
+					
+						IF FASE = "00" THEN
+							PC   <= PC + 1;
+							FASE <= "01";						
+						ELSIF FASE = "01" THEN
+							PC     <= DOUT - 1;							
+							ESTADO <= EXECUTA;							
+						END IF;		
+						
+					WHEN "00111011" => -- JZ 3B
+					
+						IF FASE = "00" THEN
+							PC <= PC + 1;
+							FASE <= "01";
+							
+						ELSIF FASE = "01" THEN
+							IF A = 0 THEN
+								PC <= DOUT - 1;
+							END IF;							
+							ESTADO <= EXECUTA;
+						END IF;					
+						
+					WHEN "00111100" => -- JNZ 3C
+						IF FASE = "00" THEN
+							PC   <= PC + 1;
+							FASE <= "01";
+						ELSIF FASE = "01" THEN
+							IF A /= 0 THEN
+								PC <= DOUT - 1;
+							END IF;
+							ESTADO <= EXECUTA;
+						END IF;
+						
+					WHEN "01111010" =>
+						A <= A - 1;
+						ESTADO <= EXECUTA;
+						
+					
+					
+					
+					
 					WHEN OTHERS => NULL;
 
 				END CASE; 
